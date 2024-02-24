7c086f7ffe6a6da863f84382cc1ec966322368fd
aldimf26 <aldimf26@gmail.com>
2024-02-24 23:33:04 +0800

    update dulu

--- components/card.vue
+++ components/card.vue
@@ -1,28 +1,51 @@
 <template>
   <!-- component -->
-  <div class="max-w-sm mx-auto my-8">
-    <div
-      style="
-        background-image: url(https://images.unsplash.com/photo-1539185441755-769473a23570?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1951&q=80;
-      "
-      class="bg-gray-300 h-64 w-full rounded-lg shadow-md bg-cover bg-center"
-    ></div>
-    <div
-      class="w-56 md:w-64 bg-white -mt-10 shadow-lg rounded-lg overflow-hidden"
-    >
-      <div
-        class="py-2 text-center font-bold uppercase tracking-wide text-gray-800"
+  <div v-for="d in 3" class="mx-8 mb-10 bg-lime-100  mt-5 flex border-4 rounded-3xl py-8 px-16">
+    <div class="w-1/2 p-6">
+      <img
+        x-intersect="$el.classList.add('scale')"
+        class="shadow-sm rounded-lg scale"
+        src="https://aldi.agaject.com/img/upperclass.png"
+        alt="Gambar dengan shadow dan glow"
+      />
+    </div>
+    <div class="w-1/2 p-6">
+      <h2
+        x-intersect="$el.classList.add('fadeInUp')"
+        class="text-2xl font-bold mb-4 text-primary fadeInUp"
       >
-        Nike Revolt
+        Upperclass Indonesia
+      </h2>
+      <p
+        x-intersect="$el.classList.add('fadeInUp')"
+        class="text-secondary leading-relaxed fadeInUp"
+      >
+        a beauty sales website that sells beauty products for hair
+      </p>
+
+      <div x-intersect="$el.classList.add('fadeInUp')" class="flex fadeInUp">
+        <div class="w-1/2 p-6">
+          <p>
+            <i class="fa-regular fa-circle-check text-btnBg mt-5"></i> Laravel
+          </p>
+
+          <p>
+            <i class="fa-regular fa-circle-check text-btnBg mt-5"></i> Jquery
+          </p>
+        </div>
+        <div class="w-1/2 p-6">
+          <p>
+            <i class="fa-regular fa-circle-check text-btnBg mt-5"></i> Bootstrap
+          </p>
+        </div>
       </div>
-      <div class="flex items-center justify-between py-2 px-3 bg-gray-400">
-        <h1 class="text-gray-800 font-bold">$129</h1>
-        <button
-          class="bg-gray-800 text-xs text-white px-2 py-1 font-semibold rounded uppercase hover:bg-gray-700"
-        >
-          Add to cart
-        </button>
-      </div>
+      <a
+        x-intersect="$el.classList.add('scale')"
+        href="https://upperclassindonesia.com/"
+        target="_blank"
+        class="leading-relaxed rounded hover:bg-primary bg-baju text-hitam border border-spacing-4 py-2 px-2 scale"
+        ><i class="fa-solid fa-link"></i> View Demo</a
+      >
     </div>
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
 
 <template>
   <!-- tampilan desktop -->
-  <div class="hidden md:block">
+  <div class="hidden lg:block">
     <div class="flex justify-center items-center">
       <img
         src="~/public/animasi/13.png"
@@ -43,7 +43,7 @@
   </div>
 
   <!-- tampilan hp -->
-  <div class="block md:hidden">
+  <div class="block lg:hidden">
     <div class="flex">
       <img
         src="~/public/animasi/13.png"
@@ -81,54 +81,7 @@
   </div>
 
   <!-- content project -->
-  <div v-for="d in 3" class="mx-8 mb-10 bg-lime-100  mt-5 flex border-4 rounded-3xl py-8 px-16">
-    <div class="w-1/2 p-6">
-      <img
-        x-intersect="$el.classList.add('scale')"
-        class="shadow-sm rounded-lg scale"
-        src="https://aldi.agaject.com/img/upperclass.png"
-        alt="Gambar dengan shadow dan glow"
-      />
-    </div>
-    <div class="w-1/2 p-6">
-      <h2
-        x-intersect="$el.classList.add('fadeInUp')"
-        class="text-2xl font-bold mb-4 text-primary fadeInUp"
-      >
-        Upperclass Indonesia
-      </h2>
-      <p
-        x-intersect="$el.classList.add('fadeInUp')"
-        class="text-secondary leading-relaxed fadeInUp"
-      >
-        a beauty sales website that sells beauty products for hair
-      </p>
-
-      <div x-intersect="$el.classList.add('fadeInUp')" class="flex fadeInUp">
-        <div class="w-1/2 p-6">
-          <p>
-            <i class="fa-regular fa-circle-check text-btnBg mt-5"></i> Laravel
-          </p>
-
-          <p>
-            <i class="fa-regular fa-circle-check text-btnBg mt-5"></i> Jquery
-          </p>
-        </div>
-        <div class="w-1/2 p-6">
-          <p>
-            <i class="fa-regular fa-circle-check text-btnBg mt-5"></i> Bootstrap
-          </p>
-        </div>
-      </div>
-      <a
-        x-intersect="$el.classList.add('scale')"
-        href="https://upperclassindonesia.com/"
-        target="_blank"
-        class="leading-relaxed rounded hover:bg-primary bg-baju text-hitam border border-spacing-4 py-2 px-2 scale"
-        ><i class="fa-solid fa-link"></i> View Demo</a
-      >
-    </div>
-  </div>
+  
 
   <!-- <div class="grid grid-cols-3">
     <div class="cursor-pointer bg-cyan-400 rounded-2xl h-64 m-5">
