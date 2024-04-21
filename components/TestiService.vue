f8beae27c29623b9dd4a1a929b85666566ca82c1
aldimf26 <aldimf26@gmail.com>
2024-04-22 00:04:07 +0800

    update service

--- components/TestiService.vue
+++ components/TestiService.vue
@@ -1,25 +1,63 @@
 <template>
   <div class="py-6 w-full max-w-5xl mx-auto px-4 sm:px-6 md:px-8">
     <div class="grid grid-cols-2 md:grid-cols-4 gap-1">
-      <div class="bg-orange-200 h-56 rounded-tr-3xl rounded-b-3xl">
-        <Garis/>
+      <div
+        class="relative overflow-hidden bg-orange-200 h-56 rounded-tr-3xl rounded-b-3xl"
+      >
+        <Garis />
+        <span>Web Developer</span>
+        <div class="mt-1 grid grid-row gap-1">
+          <TestiList konten="VS Code" />
+          <TestiList konten="Git" />
+          <TestiList konten="Terminal" />
+        </div>
+
+        <img
+          class="absolute top-30 z-10 rounded-3xl p-1 mt-2 w-full"
+          src="~/public/koding.jpg"
+          alt=""
+        />
       </div>
       <div class="bg-amber-200 relative h-56 rounded-bl-3xl rounded-t-3xl">
-        <Garis/>
-        <img class="absolute top-16" src="~/public/rich.png" alt="">
+        <Garis />
+        <span>Tech</span>
+        <div class="mt-1 grid grid-row gap-1">
+          <TestiList konten="Laravel" />
+          <TestiList konten="Livewire" />
+          <TestiList konten="Nuxt 3" />
+          <TestiList konten="Jquery" />
+          <TestiList konten="Tailwind" />
+          <TestiList konten="Bootstrap" />
+        </div>
       </div>
     </div>
     <div class="grid grid-cols-1 py-1">
-      <div class="bg-amber-100 h-24 rounded-full">Plan for the day</div>
+      <div class="relative bg-amber-100 h-24 rounded-full">
+        Plan for the day
+      </div>
     </div>
     <div class="grid grid-cols-2 md:grid-cols-4 gap-1">
-      <div class="bg-[#F47F6B] h-56 rounded-tr-3xl rounded-b-3xl">
-                <Garis/>
-
+      <div class="relative bg-cyan-200 h-56 rounded-tr-3xl rounded-b-3xl">
+        <Garis />
+        <span>Tools</span>
+        <div class="mt-1 grid grid-row gap-1">
+          <TestiList konten="FL Studio 20" />
+          <TestiList konten="Capcut" />
+          <TestiList konten="Canva" />
+          <TestiList konten="Filmora" />
+          <TestiList konten="OBS Studio" />
+          <TestiList konten="Pics Art" />
+        </div>
       </div>
-      <div class="bg-[#FFB32B] h-56 rounded-bl-3xl rounded-t-3xl">
-                <Garis/>
-
+      <div class="relative overflow-hidden bg-green-400 h-56 rounded-bl-3xl rounded-t-3xl">
+        <Garis />
+        <span>Music Maker</span>
+        <div class="mt-1 grid grid-row gap-1">
+            <TestiList konten="Trap Beat" />
+            <TestiList konten="Remix" />
+            <TestiList class="z-20" konten="EDM" />
+        </div>
+        <img class="absolute top-28 z-10" src="~/public/rich.png" alt="" />
       </div>
     </div>
   </div>
